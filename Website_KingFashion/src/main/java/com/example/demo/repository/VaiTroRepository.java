d64356eecf081612cfd65622f28b6874d776eb79
hungdmph23557 <hungdmph23557@fpt.edu.vn>
2023-11-16 22:35:27 +0700

    update

--- Website_KingFashion/src/main/java/com/example/demo/repository/VaiTroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.VaiTro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface VaiTroRepository extends JpaRepository<VaiTro, UUID> {
-}
